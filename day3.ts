aa2de7009ca4683b4de924f26532d8f2d093e46a
danieloeyss <danielf2721@gmail.com>
2024-10-03 13:18:59 +0700

    exercise time

---
+++ day3.ts
@@ -0,0 +1,47 @@
+// Soal 1
+const angkaSoal1: number = 9;
+let barisSoal1: number = 10;
+for (let i = 0; i < barisSoal1+1; i++){
+    console.log(`${angkaSoal1} x ${i} = ${angkaSoal1*i}`);
+}
+
+// Soal 2
+const kataSoal2: string= "Madam";
+let balik = "";
+for (let i = kataSoal2.length-1;i >= 0; i--){
+    balik += kataSoal2[i]
+}
+if (balik.toLowerCase() == kataSoal2.toLowerCase()){
+    console.log(`${kataSoal2} Termasuk Palindrom `);
+    
+}else{
+    console.log(`Not a Palindrom`);   
+}
+
+// Soal 3
+const angkaSoal3: number = 10000;
+let hasilSoal3: number = angkaSoal3/10000 ;
+console.log(`${angkaSoal3} dalam bentuk kilometer adalah ${hasilSoal3} Km`);
+
+// Soal 4
+
+
+// Soal 5
+
+
+
+// Soal 6
+
+
+// Soal 7
+
+
+// Soal 8 
+
+
+// Soal 9
+
+// Soal 10 
+
+
+// Soal 11
